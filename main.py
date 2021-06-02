e6a184b6c14cac527c5135e1642eb4acfdbe9a9e
Manuel Heredia <manolo.heredia@gmail.com>
2021-06-02 22:31:32 +0200

    Add enemy, collision logic and sound

--- main.py
+++ main.py
@@ -7,22 +7,50 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
 from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+
+
+def collides(rect1, rect2):
+    # COLLIDES implements basic AABB (axis aligned bounding box) collission detection)
+    r1x = rect1[0][0]
+    r1y = rect1[0][1]
+    r2x = rect2[0][0]
+    r2y = rect2[0][1]
+    r1w = rect1[1][0]
+    r1h = rect1[1][1]
+    r2w = rect2[1][0]
+    r2h = rect2[1][1]
+
+    if r1x < (r2x + r2w) and (r1x + r1w) > r2x and r1y < (r2y + r2h) and (r1y + r1h) > r2y:
+        return True
+    else:
+        return False
 
 
 class GameWidget(Widget):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # initialize keyboard input
         self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
         self._keyboard.bind(on_key_up=self._on_key_up)
 
+        # Rendering: add instructions to the canvas attribute of the Widget
         with self.canvas:
-            self.player = Rectangle(source='./player.png', pos=(0, 0), size=(100, 100))
+            self.player = Rectangle(source='./assets/player.png', pos=(0, 0), size=(100, 100))
+            self.enemy = Rectangle(source='./assets/enemy.png', pos=(400, 400), size=(100, 100))
 
+        # Listen for keyboard strokes
         self.keysPressed = set()
 
-        Clock.schedule_interval(self.move_step, 0)  # refresh every frame
+        # Scheduler to call refresh regularly (here 0 = at every frame)
+        Clock.schedule_interval(self.move_step, 0)
+
+        # play background music
+        self.sound = SoundLoader.load("./assets/thunder.ogg")
+        self.sound.play()
 
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self.on_key_down)
@@ -38,6 +66,8 @@ class GameWidget(Widget):
             self.keysPressed.remove(text)
 
     def move_step(self, dt):
+        # MOVE_STEP: movement and collision logic
+
         x = self.player.pos[0]
         y = self.player.pos[1]
 
@@ -54,11 +84,16 @@ class GameWidget(Widget):
 
         self.player.pos = (x, y)
 
+        if collides((self.player.pos, self.player.size),
+                    (self.enemy.pos, self.enemy.size)):
+            print("Gotcha!")
+        else:
+            print('ok')
+
 
 class MyApp(App):
     def build(self):
         return GameWidget()
-        # return Label(text='Hello world!')
 
 
 if __name__ == "__main__":
